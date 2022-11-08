e6a7af133df0363039422c1262f246353ac23caa
breezjin <breezjin@gmail.com>
2022-11-08 08:00:29 +0000

    Update from https://github.com/POODADAK/poodadak-client/commit/226ac10425784a912302266042be0ab196858443

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd ../
 mkdir output
+mkdir output_temp
 cp -R ./[your-repository-name]/* ./output
+cp -R ./[your-repository-name]/* ./output_temp
 cp -R ./output ./[your-repository-name]/
+cp -R ./output_temp ./[your-repository-name]/
